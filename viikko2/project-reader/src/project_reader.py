1b94b82ec6c76fb7d81d31b55c917030b07b28ca
Sasha Usoskin <sashausoskin@live.com>
2022-10-25 14:25:08 +0300

    ADD viikko 2 tehtävät

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,17 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed_toml = toml.loads(content)
+        print(parsed_toml)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(parsed_toml["tool"]["poetry"]["name"], parsed_toml["tool"]["poetry"]["description"], parsed_toml["tool"]["poetry"]["dependencies"], parsed_toml["tool"]["poetry"]["dev-dependencies"])

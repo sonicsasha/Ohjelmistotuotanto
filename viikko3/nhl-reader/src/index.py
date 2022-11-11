b059c5b7dd38905e2e4c33b55940c0de5ed11fdc
Sasha Usoskin <sashausoskin@live.com>
2022-11-11 12:17:30 +0200

    FIX old link
    lisätty uusi linkki tehtävään 1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -4,7 +4,7 @@ from playerreader import PlayerReader
 from playerstats import PlayerStats
 
 def main():
-    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
     players = stats.top_scorers_by_nationality("FIN")

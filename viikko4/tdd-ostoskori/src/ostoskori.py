67889454e5f2ffef7d926a78fb8badd72b1a89c5
Sasha Usoskin <sashausoskin@live.com>
2022-11-18 15:01:28 +0200

    ADD Yhden tuotteen lisäämisen jälkeen ostoskori sisältää yhden ostoksen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,13 +3,13 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self._ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         maara = 0
 
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             maara += ostos.lukumaara()
         
         return maara
@@ -20,19 +20,19 @@ class Ostoskori:
     def hinta(self):
         summa = 0
 
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             summa += ostos.tuote.hinta() * ostos.lukumaara()
         
         return summa
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             if ostos.tuote.nimi() == lisattava.nimi():
                 ostos.muuta_lukumaaraa(1)
                 return
         
-        self.ostokset.append(Ostos(lisattava))
+        self._ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -43,6 +43,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self._ostokset
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
